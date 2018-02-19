4dfe3668e13995864ab5de9861fef190c1298502
tigoe <tom.igoe@gmail.com>
2018-02-19 15:29:52 -0500

    updates to sACNIntro

--- examples/sACNIntro/sACNIntro.ino
+++ examples/sACNIntro/sACNIntro.ino
@@ -14,13 +14,14 @@
    3 - green
    4 - blue
 
-   You'll also need to add a tab to your sketch called config.h
+   You'll also need to add a tab to your sketch called arduino_secrets.h
    for the SSID and password of the network to which you plan to connect,
    as follows:
-   char ssid[]     = "network";  // fill in your value
-    char password[] = "password"; // fill in your value
+   #define SECRET_SSID "ssid"  // fill in your value
+  #define SECRET_PASS "password" // fill in your value
 
    created 17 Jan 2018
+   updated 19 Feb 2018
    by Tom Igoe
 
 */
@@ -59,10 +60,10 @@ void setup() {
   // set DMX channel values to 0:
   for (int dmxChannel = 1; dmxChannel < 513; dmxChannel++) {
     myController.setChannel(dmxChannel, 0);
-    myController.sendPacket(receiverAddress);
   }
+  myController.sendPacket(receiverAddress);
   // set green and blue channels to make a teal hue:
-   myController.setChannel(1, 145);
+  myController.setChannel(1, 145);
   myController.setChannel(2, 80);
 }
 
@@ -75,7 +76,7 @@ void loop() {
     delay(100);                                    // wait .1 second
   }
   delay(1000);
-    // fade down:
+  // fade down:
   for (int level = 255; level >= 0; level--) {
     myController.setChannel(3, level);              // set channel 1 (brightness)
     Serial.println(level);                          // print level
