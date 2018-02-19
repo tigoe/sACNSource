7d8dfa8f555c6da09f765ec98e19f8ea8263be8d
tigoe <tom.igoe@gmail.com>
2018-02-19 15:18:58 -0500

    updates sACNIntro to use arduino_secrets.h

--- examples/sACNIntro/sACNIntro.ino
+++ examples/sACNIntro/sACNIntro.ino
@@ -1,35 +1,35 @@
- /*
- *  sACN intro 
- *  
- *  This sketch creates a sACN packet, then uses it to send 
- *  fade information to an sACN receiver. 
- *  This was originally tested with a DMXKing eDMX1 Pro
- *  and a Coemar LEDko Revlection series spot.
- *  https://dmxking.com/artnetsacn/edmx1-pro
- *  http://www.coemar.com/bin/index.php?id=22&serie=2&lng=en&tipologia=1
- *  
- *  Channels used on the LEDko reflection:
- *  1 - brightness
- *  2 - red
- *  3 - green
- *  4 - blue
- *  
- *  You'll also need to add a tab to your sketch called config.h
- *  for the SSID and password of the network to which you plan to connect,
- *  as follows:
- *  char ssid[]     = "network";  // fill in your value
-*   char password[] = "password"; // fill in your value
- *  
- *  created 17 Jan 2018
- *  by Tom Igoe
- *  
- */
+/*
+   sACN intro
+
+   This sketch creates a sACN packet, then uses it to send
+   fade information to an sACN receiver.
+   This was originally tested with a DMXKing eDMX1 Pro
+   and a Coemar LEDko Revlection series spot.
+   https://dmxking.com/artnetsacn/edmx1-pro
+   http://www.coemar.com/bin/index.php?id=22&serie=2&lng=en&tipologia=1
+
+   Channels used on the LEDko reflection:
+   1 - brightness
+   2 - red
+   3 - green
+   4 - blue
+
+   You'll also need to add a tab to your sketch called config.h
+   for the SSID and password of the network to which you plan to connect,
+   as follows:
+   char ssid[]     = "network";  // fill in your value
+    char password[] = "password"; // fill in your value
+
+   created 17 Jan 2018
+   by Tom Igoe
+
+*/
 #include <SPI.h>
 #include <WiFi101.h>
 //#include <ESP8266WiFi.h>    // This should work with the ESP8266 as well.
 #include <WiFiUdp.h>
 #include <sACNSource.h>
-#include "config.h"
+#include "arduino_secrets.h"
 
 WiFiUDP Udp;                                  // instance of UDP library
 sACNSource myController(Udp);                 // Your Ethernet-to-DMX device
@@ -38,15 +38,14 @@ char receiverAddress[] = "192.168.0.12";      // sACN receiver address
 int myUniverse = 1;                                 // DMX universe
 char myDevice[] = "myDeviceName";                   // sender name
 char myUuid[] = "130edd1b-2d17-4289-97d8-2bff1f4490fb"; // sender UUID
-byte level;                                         // DMX channel level
 
 void setup() {
   Serial.begin(9600);
   //  while you're not connected to a WiFi AP,
   while ( WiFi.status() != WL_CONNECTED) {
     Serial.print("Attempting to connect to Network named: ");
-    Serial.println(ssid);           // print the network name (SSID)
-    WiFi.begin(ssid, password);     // try to connect
+    Serial.println(SECRET_SSID);           // print the network name (SSID)
+    WiFi.begin(SECRET_SSID, SECRET_PASS);     // try to connect
     delay(2000);
   }
   // initialize sACN source:
@@ -56,22 +55,34 @@ void setup() {
   IPAddress ip = WiFi.localIP();
   Serial.print("IP Address: ");
   Serial.println(ip);
-  
+
   // set DMX channel values to 0:
   for (int dmxChannel = 1; dmxChannel < 513; dmxChannel++) {
     myController.setChannel(dmxChannel, 0);
+    myController.sendPacket(receiverAddress);
   }
   // set green and blue channels to make a teal hue:
-  myController.setChannel(3, 145);
-  myController.setChannel(4, 80);
+   myController.setChannel(1, 145);
+  myController.setChannel(2, 80);
 }
 
 void loop() {
-  myController.setChannel(1, level);              // set channel 1 (brightness)
-  level = level + 10;                             // increment level
-  Serial.println(level);                          // print level
-  myController.sendPacket(receiverAddress); // send the data
-  delay(1000);                                    // wait 1 second
+  // fade up:
+  for (int level = 0; level < 256; level++) {
+    myController.setChannel(3, level);              // set channel 1 (brightness)
+    Serial.println(level);                          // print level
+    myController.sendPacket(receiverAddress);       // send the data
+    delay(100);                                    // wait 1 second
+  }
+  delay(1000);
+    // fade down:
+  for (int level = 255; level >= 0; level--) {
+    myController.setChannel(3, level);              // set channel 1 (brightness)
+    Serial.println(level);                          // print level
+    myController.sendPacket(receiverAddress);      // send the data
+    delay(100);                                    // wait 1 second
+  }
+  delay(1000);
 }
 
 
