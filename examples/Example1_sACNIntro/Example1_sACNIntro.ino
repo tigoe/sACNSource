a478574b7f3c1fa83f06a499fc78dc244aae6cec
Tom Igoe <tom.igoe@gmail.com>
2023-08-26 13:05:19 -0400

    corrected typos

--- examples/Example1_sACNIntro/Example1_sACNIntro.ino
+++ examples/Example1_sACNIntro/Example1_sACNIntro.ino
@@ -12,7 +12,7 @@
    as follows:
    #define SECRET_SSID "ssid"  // fill in your value
   #define SECRET_PASS "password" // fill in your value
-  #define SECRET_SACN_RECV "192.168.0.14"  // your sACM receiver's IP address
+  #define SECRET_SACN_RECV "192.168.0.14"  // your sACN receiver's IP address
   // Unique ID of your SACN source. You can generate one from https://uuidgenerator.net
   // or on the command line by typing uuidgen
   #define SECRET_SACN_UUID "CBC0C271-8022-4032-BC6A-69F614C62816"
--- examples/Example1_sACNIntro/arduino_secrets.h
+++ examples/Example1_sACNIntro/arduino_secrets.h
@@ -1,6 +1,6 @@
 #define SECRET_SSID "ssid"            // your network name
 #define SECRET_PASS "password"        // your network password
-#define SECRET_SACN_RECV "192.168.0.14"  // your sACM receiver's IP address
+#define SECRET_SACN_RECV "192.168.0.14"  // your sACN receiver's IP address
 // Unique ID of your SACN source. You can generate one from https://uuidgenerator.net
 // or on the command line by typing uuidgen
 #define SECRET_SACN_UUID "CBC0C271-8022-4032-BC6A-69F614C62816"             
